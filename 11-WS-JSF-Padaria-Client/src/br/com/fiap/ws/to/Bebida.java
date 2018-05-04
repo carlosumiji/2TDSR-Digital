fb2c2ab1f452b6f034b7bda23e0cdec1bae6807c
logonrm <logonrm@fiap.com.br>
2018-05-03 22:34:00 -0300

    terminado o projeto 10 de client

--- 11-WS-JSF-Padaria-Client/src/br/com/fiap/ws/to/Bebida.java
+++ 11-WS-JSF-Padaria-Client/src/br/com/fiap/ws/to/Bebida.java
@@ -4,54 +4,54 @@ import java.util.Calendar;
 
 public class Bebida {
 
-		private int codigo;
-		
-		private String nome;
-		
-		private Calendar dataValidade;
-		
-		private boolean alcoolico;
-		
-		private double preco;
+	private int codigo;
+	
+	private String nome;
+	
+	private Calendar dataValidade;
+	
+	private boolean alcoolico;
+	
+	private double preco;
 
-		public int getCodigo() {
-			return codigo;
-		}
+	public int getCodigo() {
+		return codigo;
+	}
 
-		public void setCodigo(int codigo) {
-			this.codigo = codigo;
-		}
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
 
-		public String getNome() {
-			return nome;
-		}
+	public String getNome() {
+		return nome;
+	}
 
-		public void setNome(String nome) {
-			this.nome = nome;
-		}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 
-		public Calendar getDataValidade() {
-			return dataValidade;
-		}
+	public Calendar getDataValidade() {
+		return dataValidade;
+	}
 
-		public void setDataValidade(Calendar dataValidade) {
-			this.dataValidade = dataValidade;
-		}
+	public void setDataValidade(Calendar dataValidade) {
+		this.dataValidade = dataValidade;
+	}
 
-		public double getPreco() {
-			return preco;
-		}
+	public double getPreco() {
+		return preco;
+	}
 
-		public void setPreco(double preco) {
-			this.preco = preco;
-		}
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
 
-		public boolean isAlcoolico() {
-			return alcoolico;
-		}
+	public boolean isAlcoolico() {
+		return alcoolico;
+	}
 
-		public void setAlcoolico(boolean alcoolico) {
-			this.alcoolico = alcoolico;
-		}
-		
+	public void setAlcoolico(boolean alcoolico) {
+		this.alcoolico = alcoolico;
+	}
+	
 }
